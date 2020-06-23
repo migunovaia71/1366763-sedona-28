a78dd0363dde8ec8565c024469fea2edf56d6a0a
migunovaia71 <migunovaia71@gmail.com>
2020-06-23 15:52:51 +0300

    Сверстана главная страница

---
+++ js/search.js
@@ -0,0 +1,7 @@
+var searchBtn = document.querySelector(".search-btn1");
+var searchForm = document.querySelector(".search");
+
+searchBtn.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    searchForm.classList.toggle("search-close");
+});
